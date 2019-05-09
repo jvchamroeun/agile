e4bf9636fce7db8422b7e895e1583f6061bc6dc3
Jason <j.soukchamroeun@gmail.com>
2019-05-09 13:56:37 -0700

    Jason 1st TDD commit

--- test/projectTest.js
+++ test/projectTest.js
@@ -6,7 +6,26 @@ const expect = require('chai').expect;
 const mongoose = require('mongoose');
 const DB_URI = 'mongodb://localhost:27017/accounts';
 const TEST_URI = 'mongodb://localhost:27017/test';
+const passport = require('passport');
+var express = require('express');
 
+var cheerio = require("cheerio");
+const nock = require("nock");
+var moment = require("moment");
+
+
+
+var accountSchema=({
+    firstname: String,
+    lastname: String,
+    username: String,
+    password: String,
+    type: String,
+    cash2: Array,
+    stocks: Array
+});
+
+var account;
 
 function connect() {
     return new Promise((resolve, reject) => {
@@ -18,6 +37,9 @@ function connect() {
                 .then(() => {
                     mongoose.connect(DB_URI,
                         { useNewUrlParser: true, useCreateIndex: true })
+                        .then((db) => {
+                            account = db.model('test', accountSchema)
+                        })
                         .then((res, err) => {
                             if (err) return reject(err);
                             resolve();
@@ -45,6 +67,27 @@ chai.use(chaiHttp);
 const app = require('../project');
 const utils = require('../utils');
 
+
+var agent = chai.request.agent(app);
+describe("GET /trading-success", function () {
+
+    it("Successful log in", function (done) {
+        agent
+            .post("/login")
+            .send({
+                _method: "post",
+                username: "MaxiMaxr",
+                password: "apple12345"
+            })
+            .then(function (res) {
+                var exp = "/trading-success";
+                expect(res.req.path).to.equal(exp);
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
+});
+
 describe('GET /', function () {
     it("should return webpage with title of 'Welcome to the login page.' GET /", function (done) {
         chai.request(app)
@@ -63,10 +106,10 @@ describe('GET /', function () {
                 done()
             })
     });
+
 });
 
-
-describe('POST /', function () {
+describe('POST /register', function () {
     before((done) => {
         connect()
             .then(() => done())
@@ -126,18 +169,36 @@ describe('POST /', function () {
             .catch((err) => done(err));
     });
 
-    // it("should return webpage with title of 'Welcome to the trading page.' GET /trading-success ", (done) => {
-    //     chai.request(app).post('/login')
-    //         // .send({username:'SuperSaiyan', password:"apple123456"})
-    //         .field('username', 'SuperSaiyan')
-    //         .field('password', 'apple123456')
-    //         .then((res) => {
-    //             expect(res).to.have.status(200);
-    //             done();
-    //         })
-    //         .catch((err) => done(err));
-    // });
+    it('creating another user in MongoDB',(done) => {
+        chai.request(app).post('/register')
+            .send({firstname:'Maxi', lastname:'Maxer', username:'MaxiMaxr', password:'apple12345', confirm_password:'apple12345'})
+            .then((res) => {
+                expect(res).to.have.status(200);
+                done();
+            })
+            .catch((err) => done(err));
+    });
 
 
+    it("should create a test user in Mock-Database", function(done) {
+        account.create({
+            firstname: "Test",
+            lastname: "Tester",
+            username: "Testing",
+            password: "TestMePlease",
+            type: "standard",
+            cash2: [],
+            stocks: []
+        }, function(err) {
+            expect(err).not.to.be.ok;
+            done(err);
+        });
+    });
 
+    it("should find test user in Mock-Database", function(done) {
+        account.findOne({username: "Testing"}, function(err, result) {
+            expect(err).not.to.be.ok;
+            done(err);
+        });
+    });
 });
