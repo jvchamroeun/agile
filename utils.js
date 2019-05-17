7eca6db704c60bb4a4def797876d4caf850ff607
unknown <thomasfwong@hotmail.com>
2019-05-17 10:28:38 -0700

    5/17 Thomas

--- utils.js
+++ utils.js
@@ -1,4 +1,6 @@
+    
 const MongoClient = require('mongodb').MongoClient;
+const db_uri = "mongodb+srv://jasonadmin:jasoniscool@accounts-ij7tq.mongodb.net/accounts";
 
 var _db = null;
 
@@ -8,7 +10,7 @@ module.exports.getDb = function() {
 
 module.exports.init = function(callback) {
     // MongoClient.connect('mongodb://localhost:27017/test', function(err, client) {
-    MongoClient.connect('mongodb://localhost:27017/accounts', function(err, client) {
+    MongoClient.connect(db_uri, { useNewUrlParser: true }, function(err, client) {
         if (err) {
             return console.log('Unable to connect to DB');
         }
